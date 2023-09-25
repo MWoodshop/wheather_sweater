1f00cec7d717d3b068ef96492a99de7d6dc9b5cf
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-25 11:33:17 -0400

    Feat: updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
     namespace :v0 do
       get 'forecast', to: 'forecasts#show'
     end
+
+    namespace :v1 do
+      get 'book-search', to: 'book_search#show'
+    end
   end
 end
