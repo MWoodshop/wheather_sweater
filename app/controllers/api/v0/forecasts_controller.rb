e1555ebe4a4cd88d573d1ce96bcb2cbfdbdd4b1e
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-26 22:29:34 -0400

    Feat: fixes controller

--- app/controllers/api/v0/forecasts_controller.rb
+++ app/controllers/api/v0/forecasts_controller.rb
@@ -6,8 +6,21 @@ module Api
         coordinates = GeocodingService.get_coordinates(location)
 
         if coordinates
-          weather = WeatherService.get_weather(coordinates)
-          render json: ForecastSerializer.format(weather, coordinates)
+          eta = params[:eta]
+          weather_at_eta = eta ? WeatherService.get_weather_at_eta(coordinates, eta.to_i) : nil
+          current_weather = WeatherService.get_current_weather(coordinates)
+          daily_weather = WeatherService.get_daily_weather(coordinates)
+          hourly_weather = WeatherService.get_hourly_weather(coordinates)
+
+          weather = Weather.new(
+            current_weather:,
+            daily_weather:,
+            hourly_weather:,
+            weather_at_eta:
+          )
+
+          forecast = ForecastSerializer.format(weather, coordinates)
+          render json: forecast
         else
           render json: { error: 'Invalid location' }, status: 400
         end
